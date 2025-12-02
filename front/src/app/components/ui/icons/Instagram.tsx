fbdd3b1c8e0dc071783a6a14141491cd3ab93225
Sajad <zareisajad@protonmail.com>
2025-12-02 12:07:50 +0330

    added icons

---
+++ front/src/app/components/ui/icons/Instagram.tsx
@@ -0,0 +1,18 @@
+export default function Instagram() {
+  return (
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke="currentColor"
+  strokeWidth={1.5}
+  className="w-5 h-5"
+>
+  <rect x="2" y="2" width="20" height="20" rx="5" ry="5" />
+  <circle cx="12" cy="12" r="4" />
+  <circle cx="18" cy="6" r="1" />
+</svg>
+
+
+  );
+}
--- front/src/app/components/ui/icons/Plus.tsx
+++ front/src/app/components/ui/icons/Plus.tsx
@@ -1,6 +1,6 @@
 export default function Plus() {
   return (
-    <svg className="size-4" viewBox="0 0 24 24" fill="none" stroke="currentColor">
+    <svg className="size-7" viewBox="0 0 24 24" fill="none" stroke="currentColor">
       <path d="M12 4.5v15m7.5-7.5h-15" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
   );
